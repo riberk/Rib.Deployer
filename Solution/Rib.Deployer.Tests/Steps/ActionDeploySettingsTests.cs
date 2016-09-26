6b2b9dc90e2733a7bc4a56fbfda2560332368889
p.kurdikov <p.kurdikov@ts-soft.ru>
2016-09-26 12:04:22 +0300

    ActionDeploySettingsTests

---
+++ Solution/Rib.Deployer.Tests/Steps/ActionDeploySettingsTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rib.Deployer.Steps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rib.Deployer.Steps
+{
+    [TestClass]
+    public class ActionDeploySettingsTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ActionDeploySettingsNull1Test() => new ActionDeploySettings("name", null, () => {});
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ActionDeploySettingsNull2Test() => new ActionDeploySettings("name", () => { }, null);
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ActionDeploySettingsNull3Test() => new ActionDeploySettings(null, () => { }, () => {});
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ActionDeploySettingsEmptyTest() => new ActionDeploySettings(string.Empty, () => { }, () => { });
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ActionDeploySettingsWhiteSpaceTest() => new ActionDeploySettings("      ", () => { }, () => { });
+    }
+}
