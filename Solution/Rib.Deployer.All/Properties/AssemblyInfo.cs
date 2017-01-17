e934a51776948f4ef7672fe1f78164105203d6ab
p.kurdikov <p.kurdikov@ts-soft.ru>
2017-01-17 17:13:09 +0300

    version 1.1.4

--- Solution/Rib.Deployer.All/Properties/AssemblyInfo.cs
+++ Solution/Rib.Deployer.All/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.3")]
-[assembly: AssemblyFileVersion("1.1.3")]
+[assembly: AssemblyVersion("1.1.4")]
+[assembly: AssemblyFileVersion("1.1.4")]
