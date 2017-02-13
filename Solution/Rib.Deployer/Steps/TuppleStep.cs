6d4c83ca334e5a1650246ba973f722845919dfc9
p.kurdikov <p.kurdikov@ts-soft.ru>
2017-02-13 18:24:04 +0300

    wip

--- Solution/Rib.Deployer/Steps/TuppleStep.cs
+++ Solution/Rib.Deployer/Steps/TuppleStep.cs
@@ -3,8 +3,60 @@ namespace Rib.Deployer.Steps
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Security.Cryptography;
     using Common.Logging;
     using JetBrains.Annotations;
+
+    public class CspContainerExportSettings : IStepSettings
+    {
+        public CspContainerExportSettings([NotNull] string name,
+                                          bool includePrivate,
+                                          [NotNull] string containerName,
+                                          CspProviderFlags cspProviderFlags)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(name));
+            if (string.IsNullOrWhiteSpace(containerName))
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(containerName));
+            Name = name;
+            IncludePrivate = includePrivate;
+            ContainerName = containerName;
+            CspProviderFlags = cspProviderFlags;
+        }
+
+        public string ContainerName { get; }
+
+        public bool IncludePrivate { get; }
+
+        public CspProviderFlags CspProviderFlags { get; }
+
+        public string Name { get; }
+    }
+
+    public class CspContainerExportStep : DeployStepBase<CspContainerExportSettings>
+    {
+        public CspContainerExportStep([NotNull] CspContainerExportSettings settings) : base(settings)
+        {
+        }
+
+        public CspContainerExportStep([NotNull] CspContainerExportSettings settings, ILog logger) : base(settings, logger)
+        {
+        }
+
+        public override void Apply()
+        {
+            var csp = new CspParameters
+            {
+                Flags = CspProviderFlags.UseExistingKey | Settings.CspProviderFlags
+            };
+            throw new NotImplementedException();
+        }
+
+        public override void Rollback()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 
     public class TuppleStep : DeployStepBase<TuppleSettings>, IDisposable
     {
@@ -15,6 +67,19 @@ namespace Rib.Deployer.Steps
         {
         }
 
+        /// <summary>
+        ///     Финализировать шаг. Вызывается после применения всех шагов
+        /// </summary>
+        public void Dispose()
+        {
+            foreach (var step in _invoked)
+            {
+                Logger.Info($"Tupple step {step.Name} closing");
+                var disposable = step as IDisposable;
+                disposable?.Dispose();
+                Logger.Info($"Tupple step {step.Name} closed");
+            }
+        }
 
 
         /// <summary>Применить шаг</summary>
@@ -46,20 +111,6 @@ namespace Rib.Deployer.Steps
                 Logger.Info($"Tupple step {step.Name} rolling back");
                 step.Rollback();
                 Logger.Info($"Tupple step {step.Name} rolled back");
-            }
-        }
-
-        /// <summary>
-        ///     Финализировать шаг. Вызывается после применения всех шагов
-        /// </summary>
-        public void Dispose()
-        {
-            foreach (var step in _invoked)
-            {
-                Logger.Info($"Tupple step {step.Name} closing");
-                var disposable = step as IDisposable;
-                disposable?.Dispose();
-                Logger.Info($"Tupple step {step.Name} closed");
             }
         }
 
